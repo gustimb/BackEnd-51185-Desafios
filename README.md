6bcef84b70c3d5e2385954e77cec3d3b031ecf16
gustimb <brizuelagm@outlook.com>
2023-04-20 05:03:25 -0300

    Motores_de_plantillas+Websocket
    Motores_de_plantillas+Websocket

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BackEnd-51185-Desafios
- 
+# BackEnd-51185-Desafíos
+## _Motores de plantillas + Websocket_
---
+++ src/routes/views.router.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('home')
+})
+
+router.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts')
+})
+
+export default router;
+
